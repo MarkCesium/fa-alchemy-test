1f0015c15cb36fc2541782779a2cdfd2e44ba699
MarkCesium <cesiummark01@gmail.com>
2024-03-05 20:24:36 +0300

    Refactoring config.py and docker-compose.yaml

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,15 @@
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv("...")
 
-db_url: str = getenv("DB_URL")
+def get_db_url() -> str:
+    load_dotenv()
+
+    db_user: str = getenv("DB_USER")
+    db_pass: str = getenv("DB_PASS")
+    db_name: str = getenv("DB_NAME")
+
+    return f"postgresql+asyncpg://{db_user}:{db_pass}@0.0.0.0:5432/{db_name}"
+
+
+db_url: str = get_db_url()
